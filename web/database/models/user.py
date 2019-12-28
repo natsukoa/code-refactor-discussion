96a4e93ec7aed7c07b468d4d5bb2fb9f049e5935
maaya <huki.j37@gmail.com>
2019-12-28 15:50:49 +0900

    create user.py

--- web/database/models/user.py
+++ web/database/models/user.py
@@ -1,1 +1,16 @@
 # TODO:  ここにSQLAlchemyのモデルを書く
+#from flask import g
+#from flaskr.db import get_db
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#db = get_db()
+
+class User(db.Model):
+
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
